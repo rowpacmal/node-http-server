f9276f1a350bcacfd14a2a5d882bdcb61de8ffb5
rowpacmal <rowpacmaldev@gmail.com>
2025-09-13 12:59:28 +0200

    feat(server): added minimal HTTP server base

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+function requestHandler(req: IncomingMessage, res: ServerResponse) {
+  const method = req.method ?? 'GET';
+  const url = req.url ?? '/';
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.end(`Method: ${method}\nURL: ${url}`);
+}
+
+const server = createServer(requestHandler);
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
